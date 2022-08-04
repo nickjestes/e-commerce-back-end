f93bfa64fe3a1e02f153451f6b1e54e30a807360
nickjestes <84655404+nickjestes@users.noreply.github.com>
2022-08-04 16:10:33 -0700

    fixed code, missing functionality

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'path/to/.env' });
 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        dialectOptions: {
-            decimalNumbers: true,
-        },
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      port: 3001,
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
     });
 
 module.exports = sequelize;
